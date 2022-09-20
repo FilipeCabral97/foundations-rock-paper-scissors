a63cab926813abd2f5486d5f109734b96a752f01
Filipe Cabral <rui.filipe.cabral.97@gmail.com>
2022-09-20 12:07:25 +0100

    Create UI

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let myScore = 0;
+let computerScore = 0;
+let currentRound = 1;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
@@ -20,53 +24,79 @@ function getPlayerSelection() {
   }
 }
 
-function playRound() {
-  const myChoice = getPlayerSelection();
+function playRound(e) {
+  const myChoice = e.target.innerText.toLowerCase();
   const computerChoice = getComputerChoice();
 
-  if (myChoice === computerChoice) {
-    let round = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
-    return round + "Draw!";
-  } else if (myChoice === "rock" && computerChoice === "paper") {
-    let round = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
-    return round + "You Lose! Paper beats Rock";
-  } else if (myChoice === "rock" && computerChoice === "scissors") {
-    let round = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
-    return round + "You Win! Rock beats Scissors";
-  } else if (myChoice === "paper" && computerChoice === "rock") {
-    let round = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
-    return round + "You Win! Paper beats Rock";
-  } else if (myChoice === "paper" && computerChoice === "scissors") {
-    let round = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
-    return round + "You Lose! Scissors beats Paper";
-  } else if (myChoice === "scissors" && computerChoice === "rock") {
-    let round = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
-    return round + "You Lose! Rock beats Scissors";
-  } else if (myChoice === "scissors" && computerChoice === "paper") {
-    let round = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
-    return round + "You Win! Scissors beats Paper";
-  }
-}
-
-function game() {
-  let myScore = 0;
-  let computerScore = 0;
-
-  for (let i = 1; i <= 5; i++) {
-    // console.log(`ROUND ${i}:\n` + playRound());
-    let log = `ROUND ${i}:\n` + playRound();
-    console.log(log);
-    if (log.includes("You Win!")) {
-      myScore++;
-    } else if (log.includes("You Lose!")) {
+  console.log(currentRound);
+  if (currentRound <= 5) {
+    roundHeader.innerText = "Round " + currentRound;
+    if (myChoice === computerChoice) {
+      let roundInfo = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
+      resultParagraph.innerText = roundInfo + "Draw!";
+    } else if (myChoice === "rock" && computerChoice === "paper") {
+      let roundInfo = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
       computerScore++;
+      resultParagraph.innerText = roundInfo + "You Lose! Paper beats Rock";
+    } else if (myChoice === "rock" && computerChoice === "scissors") {
+      let roundInfo = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
+      myScore++;
+      resultParagraph.innerText = roundInfo + "You Win! Rock beats Scissors";
+    } else if (myChoice === "paper" && computerChoice === "rock") {
+      let roundInfo = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
+      myScore++;
+      resultParagraph.innerText = roundInfo + "You Win! Paper beats Rock";
+    } else if (myChoice === "paper" && computerChoice === "scissors") {
+      let roundInfo = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
+      computerScore++;
+      resultParagraph.innerText = roundInfo + "You Lose! Scissors beats Paper";
+    } else if (myChoice === "scissors" && computerChoice === "rock") {
+      let roundInfo = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
+      computerScore++;
+      resultParagraph.innerText = roundInfo + "You Lose! Rock beats Scissors";
+    } else if (myChoice === "scissors" && computerChoice === "paper") {
+      let roundInfo = `PC: ${computerChoice}\nYOU: ${myChoice}\n`;
+      myScore++;
+      resultParagraph.innerText = roundInfo + "You Win! Scissors beats Paper";
     }
+    currentRound++;
   }
-  if (myScore > computerScore) {
-    console.log(`You Won the Game: ${myScore} - ${computerScore}`);
-  } else if (myScore < computerScore) {
-    console.log(`You Lost the Game: ${myScore} - ${computerScore}`);
-  } else {
-    console.log(`It's a Draw: ${myScore} - ${computerScore}`);
+  if (currentRound > 5) {
+    roundHeader.innerText = "End!";
+
+    if (myScore > computerScore) {
+      endParagraph.innerText = `You Won the Game: ${myScore} - ${computerScore}`;
+    } else if (myScore < computerScore) {
+      endParagraph.innerText = `You Lost the Game: ${myScore} - ${computerScore}`;
+    } else {
+      endParagraph.innerText = `It's a Draw: ${myScore} - ${computerScore}`;
+    }
+
+    buttons.forEach((button) => (button.style.display = "none"));
+    playAgainBtn.style.display = "";
   }
 }
+
+const reset = () => {
+  myScore = 0;
+  computerScore = 0;
+  currentRound = 1;
+  roundHeader.innerText = "Start!";
+  buttons.forEach((button) => (button.style.display = ""));
+  playAgainBtn.style.display = "none";
+  resultParagraph.innerText = "";
+  endParagraph.innerText = "";
+};
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => button.addEventListener("click", playRound));
+
+const resultsContainer = document.querySelector(".results-container");
+const resultParagraph = document.querySelector("#round-result");
+const endParagraph = document.querySelector("#end-result");
+const roundHeader = document.querySelector("#round");
+roundHeader.innerText = "Start!";
+
+const playAgainBtn = document.querySelector("#play-again-btn");
+playAgainBtn.style.display = "none";
+playAgainBtn.addEventListener("click", reset);
